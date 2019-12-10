0fa53dc7472dde3059277a57ebd67db36c490f8b
JonMaia <jonathannicolas.maia@gmail.com>
2019-12-10 16:31:21 -0300

    Traduje el nombre del titulo del navbar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AppCharacters - @yield('title')</title>
+    <title>AppPersonajes - @yield('title')</title>
     <link rel="stylesheet" 
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
@@ -12,7 +12,7 @@
 </head>
 <body>
     <nav class="navbar navbar-dark bg-primary">
-        <a href="#" class="navbar-brand">AppCharacters</a>
+        <a href="#" class="navbar-brand">AppPersonajes</a>
     </nav>
     <div class="container">
         @yield('content')
