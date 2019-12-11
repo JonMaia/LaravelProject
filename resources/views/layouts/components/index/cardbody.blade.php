6a7da32559e3111af227900efde1a23a46e0d8f4
JonMaia <jonathannicolas.maia@gmail.com>
2019-12-11 14:14:49 -0300

    Implementé la referencia a la ruta del personaje del botón más información.

--- resources/views/layouts/components/index/cardbody.blade.php
+++ resources/views/layouts/components/index/cardbody.blade.php
@@ -2,5 +2,5 @@
     <h5 class="card-title">{{$character->charactername}}</h5>
     <p class="card-text font-weight-bold">Serie o Película en la que participó:</p>
     <p>{{$character->serieorfilm}}</p>
-    <a href="#" class="btn btn-primary">Mas Información</a>
+    <a href="/characters/{{$character->id}}" class="btn btn-primary">Mas Información</a>
 </div>
